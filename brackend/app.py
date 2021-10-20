1c10b1afe51fd7de56706b010ba90f01a5d622df
Peter Robicheaux <probicheaux@gmail.com>
2021-10-20 11:56:19 -0700

    Add some dependencies

--- brackend/app.py
+++ brackend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 import logging
 logger = logging.Logger(__name__)
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/api/hello/", methods=["GET"])
 def hello():
