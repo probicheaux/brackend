e55bdd6ea5aa5e1ae35c0b445bc919c0f8807348
Peter Robicheaux <probicheaux@gmail.com>
2021-10-20 06:01:52 +0000

    We got server boys running

--- brackend/app.py
+++ brackend/app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/hello", methods=["GET"])
-def hello(name):
+@app.route("/api/hello/", methods=["GET"])
+def hello():
     return "Hello"
 
 
