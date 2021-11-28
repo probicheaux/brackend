e90471c81f15a4317582db4f6cb0daafa8b62fc5
Peter Robicheaux <probicheaux@gmail.com>
2021-11-28 07:39:09 -0800

    first brush with auth

---
+++ brackend/tasks/auth.py
@@ -0,0 +1,21 @@
+import jwt
+
+def encode_auth_token(self, user_id):
+    """
+    Generates the Auth Token
+    :return: string
+    """
+    try:
+        payload = {
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
+            'iat': datetime.datetime.utcnow(),
+            'sub': user_id
+        }
+        return jwt.encode(
+            payload,
+            app.config.get('SECRET_KEY'),
+            algorithm='HS256'
+        )
+    except Exception as e:
+        return e
+
