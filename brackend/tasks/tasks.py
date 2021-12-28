f69ff080e308fc12d562293dfad5cfe879893d3d
Peter Robicheaux <probicheaux@gmail.com>
2021-12-27 20:10:41 -0500

    Use proxies to do stuff

--- brackend/tasks/tasks.py
+++ brackend/tasks/tasks.py
@@ -1,27 +1,45 @@
 import dramatiq
 from dramatiq.brokers.redis import RedisBroker
 from firebase_admin.auth import create_user, get_user
+from flask_restful import current_app
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
-from brackend.db.models import EngineGetter, NotFoundException, Tournament, User
+from brackend.db.models import (
+    EngineGetter,
+    NotFoundException,
+    Tournament,
+    User,
+    UserRole,
+    UserTournament,
+)
 from brackend.util import BrackendException
 
 redis_broker = RedisBroker(host="redis")
 dramatiq.set_broker(redis_broker)
 
+
 def save_new_tournament(name, firebase_id):
     engine = EngineGetter.get_or_create_engine()
     with Session(engine) as session:
         session.expire_on_commit = False
-        new_tourny = Tournament(name=name)
+        new_tourney = Tournament(name=name)
         user = get_user_by_uid(firebase_id)
+        session.add(user)
         if user is None:
             raise BrackendException("Somehow a nonexistent user is trying to save a tournament")
-        new_tourny.users.append(user)
-        session.add(new_tourny)
+        session.add(new_tourney)
+        session.flush()
+        assert new_tourney.id is not None
+        new_user_tournament = UserTournament(
+            user_id=user.id, tournament_id=new_tourney.id, role=UserRole.organizer
+        )
+        session.add(new_user_tournament)
         session.commit()
-        return new_tourny
+        current_app.logger.info("Users: %s", new_tourney.users)
+        current_app.logger.info("Tournaments: %s", user.tournaments)
+        return new_tourney
+
 
 def save_new_user(username, firebase_id):
     engine = EngineGetter.get_or_create_engine()
@@ -31,6 +49,7 @@ def save_new_user(username, firebase_id):
         session.add(new_user)
         session.commit()
         return new_user
+
 
 def get_user_ids():
     engine = EngineGetter.get_or_create_engine()
@@ -49,27 +68,25 @@ def get_tournament_ids():
 def get_tournament_by_id(t_id):
     engine = EngineGetter.get_or_create_engine()
     with Session(engine) as session:
-        tournament = session\
-            .query(Tournament)\
-            .filter(Tournament.id == t_id)\
-            .one_or_none()
+        tournament = session.query(Tournament).filter(Tournament.id == t_id).one_or_none()
         return tournament
+
 
 def get_tournaments_by_uid(uid):
     engine = EngineGetter.get_or_create_engine()
     with Session(engine) as session:
-        tournaments = session\
-            .query(Tournament)\
-            .join(Tournament.users)\
-            .where(User.firebase_id == uid)\
+        tournaments = (
+            session.query(Tournament)
+            .join(Tournament.user_tournaments)
+            .join(UserTournament.user)
+            .where(User.firebase_id == uid)
             .all()
+        )
         return tournaments
+
 
 def get_user_by_uid(uid):
     engine = EngineGetter.get_or_create_engine()
     with Session(engine) as session:
-        user = session\
-            .query(User)\
-            .filter(User.firebase_id == uid)\
-            .one_or_none()
+        user = session.query(User).filter(User.firebase_id == uid).one_or_none()
         return user
